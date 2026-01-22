f074b2f80372d20d17e350b8bf405e042f3480db
rohith10e <rohith.e.s82@gmail.com>
2026-01-22 15:42:42 +0530

    feat:aws-config

---
+++ backend/config/aws-config.js
@@ -0,0 +1,12 @@
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+AWS.config.update({ region: "ap-south-1" });
+
+const s3 = new AWS.S3();
+
+const s3_bucket = process.env.S3_BUCKET;
+
+export { s3, s3_bucket };
